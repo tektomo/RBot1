1ed4a3ff0fee8c7cbd4e5c8df99e7d80753c1e1e
tektomo <taobmcoe@outlook.com>
2018-11-28 13:32:58 +0900

    いちおうできたよ

--- discordbot.py
+++ discordbot.py
@@ -1,18 +1,42 @@
 import discord
 import os
+import re
+from natto import MeCab
+import random
 
 client = discord.Client()
 token = os.environ["RBOT1_TOKEN"]
+m = MeCab()
 
+def check_msg(sentence):
+    me = m.parse(sentence)
+    list1 = re.findall(".*名詞.*\n", me)
+    list2 = []
+    for word in list1:
+        list2.append(word.split("\t"))
+    list3 = []
+    for i,word in enumerate(list2):
+        list3.append(list2[i][0])
+    
+    result = list3[-1]
+    
+    print(list1)
+    print(list2)
+    print(list3)
+
+    return result
+    
 @client.event
 async def on_ready():
     print("Login successfull!")
     
 @client.event
 async def on_message(message):
-    if message.content.startswith("にゃーん"):
-        reply = "にゃ〜ん"
-        await client.send_message(message.channel, reply)
-    
+    if client.user != message.author:
+        print (message.content)
+        result = check_msg(message.content)
+        #result = m.parse(message.content)
+        result = "今の話のテーマは" + result + "です"
+        await client.send_message(message.channel, result)
 
 client.run(token)
